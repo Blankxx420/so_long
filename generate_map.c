ecb705daca7a0d90c891175274daaca3b6ccd0fe
Blankxx420 <briceguichou.dev@gmail.com>
2024-01-03 13:24:15 +0100

    parsing map done

--- generate_map.c
+++ generate_map.c
@@ -6,7 +6,7 @@
 /*   By: blankx <blankx@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 12:54:26 by brguicho          #+#    #+#             */
-/*   Updated: 2024/01/02 01:19:30 by blankx           ###   ########.fr       */
+/*   Updated: 2024/01/03 13:23:02 by blankx           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,40 @@ void	printlinked_list(t_list **lst)
 	t_list	*tmp;
 
 	tmp = *lst;
-	while (tmp && tmp->next != NULL)
+	while (tmp)
 	{
 		printf("%s", (char*)tmp->content);
 		tmp = tmp->next;
 	}
 }
 
-void	generate_map()
+t_list	*generate_map(void)
 {
     char	*line;
 	char 	*str;
     int		fd;
 	t_list	*final_map;
 	t_list	*map;
+	
 	fd = open("map.ber", O_RDONLY);
 	final_map = malloc(sizeof(t_list));
 	if (!final_map)
 		return ;
 	line = get_next_line(fd);
+	str = ft_strdup(line);
+	final_map->content = str;
+	free(line);
+	line = get_next_line(fd);
     while (line != NULL)
-	{	
-		str = ft_strdup(line);
+	{	str = ft_strdup(line);
 		free(line);
 		map = ft_lstnew(str);
 		ft_lstadd_back(&final_map, map);
 		line = get_next_line(fd);
 	}
+	close(fd);
 	printlinked_list(&final_map);
+	return (final_map);
 }
 
 int main()
